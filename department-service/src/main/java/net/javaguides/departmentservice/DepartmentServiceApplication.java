c1c34bb264a29d42963ef63ae94f8360b5cd41c7
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-02-18 08:47:31 +0300

    Initial commit

---
+++ department-service/src/main/java/net/javaguides/departmentservice/DepartmentServiceApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.departmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartmentServiceApplication.class, args);
+	}
+
+}
